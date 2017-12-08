265709543ecf1668ceefc1d159be7049b401b3fd
Sandra Escobedo <sandra@iMac-de-Sandra.local>
2017-12-08 16:33:48 +0100

    add model test relations

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Flight, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:departured).class_name('Airport').with_foreign_key('departured_id') }
+  it { should belong_to(:arrived).class_name('Airport').with_foreign_key('arrived_id') }
 end
