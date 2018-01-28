28c95e37ec1f8138079f57cb2b27dcadf5f9bbd6
Sandra Escobedo <sandra@iMac-de-Sandra.local>
2018-01-28 18:46:42 +0100

    fix_index_view

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -4,4 +4,12 @@ class Flight < ApplicationRecord
   belongs_to :arrived, class_name: 'Airport', foreign_key: 'arrived_id'
 
   scope :by_user, ->(user) { where(user: user) }
+
+  def arrived_airport_code
+    Airport.find_by_id(arrived_id).iata_code
+  end
+
+  def departured_airport_code
+    Airport.find_by_id(departured_id).iata_code
+  end
 end
