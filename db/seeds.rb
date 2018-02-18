177c351f9d3d3c456ea854784b17509b29abd6bf
Sandra Escobedo <sescobedo@alfatecsistemas.es>
2018-02-18 20:11:17 +0100

    add seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+airport1 = Airport.new
+airport1.iata_code = 'VLC'
+airport1.address = ''
+airport1.save
+airport2 = Airport.new
+airport2.iata_code = 'BCN'
+airport2.address = ''
+airport2.save
+airport3 = Airport.new
+airport3.iata_code = 'ORY'
+airport3.address = ''
+airport3.save
+
+flight1 = Flight.new
+flight1.user = User.first
+flight1.departured = airport1
+flight1.arrived = airport2
+flight1.save
+flight2 = Flight.new
+flight2.user = User.first
+flight2.departured = airport2
+flight2.arrived = airport3
+flight2.save
+flight3 = Flight.new
+flight3.user = User.first
+flight3.departured = airport3
+flight3.arrived = airport2
+flight3.save
+flight4 = Flight.new
+flight4.user = User.first
+flight4.departured = airport1
+flight4.arrived = airport3
+flight4.save
