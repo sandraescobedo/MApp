2fbea88b3fb38d6d4d99c213ed0dd819e2c47cbe
Sandra Escobedo <sescobedo@alfatecsistemas.es>
2018-03-14 15:02:05 +0100

    validate_departured_id_and_arrived_id_are_present

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,6 +3,9 @@ class Flight < ApplicationRecord
   belongs_to :departured, class_name: 'Airport', foreign_key: 'departured_id'
   belongs_to :arrived, class_name: 'Airport', foreign_key: 'arrived_id'
 
+  validates :departured, presence: { message: I18n.t('blank') }
+  validates :arrived, presence: { message: I18n.t('blank') }
+
   scope :by_user, ->(user) { where(user: user) }
 
   def arrived_airport_code
