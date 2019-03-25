5dcee4ae089b0e5f8911d85f6dc4116f11aae319
sandra <sandraescobedobadia@gmail.com>
2019-03-25 09:55:29 +0100

    test: add airport model test

--- spec/models/airport_spec.rb
+++ spec/models/airport_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Airport, type: :model do
   it { should validate_presence_of(:iata_code) }
-  it { should have_many(:departured_flights).with_foreign_key('departured_id').inverse_of(:departured) }
-  it { should have_many(:arrived_flights).with_foreign_key('arrived_id').inverse_of(:arrived) }
+  it { should have_many(:departured_flights).with_foreign_key('departured_id')
+                                            .inverse_of(:departured) }
+  it { should have_many(:arrived_flights).with_foreign_key('arrived_id')
+                                         .inverse_of(:arrived) }
 end
